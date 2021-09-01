6d1ed6cae5de9341fec772092631d8d58f464e6b
pervejislam2404 <pervejislam2404@gmail.com>
2021-09-02 03:11:08 +0600

    update

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@ const searchButton = document.getElementById('button-search');
 const inputName = document.getElementById('search-meal');
 const container = document.getElementById('display-container');
 const msg = document.getElementById('alert-msg');
-const loader = document.getElementById('preloader');
+// const loader = document.getElementById('preloader');
+const totalData = document.getElementById('total-found')
+const showData = document.getElementById('total-show')
 
-
+let counter = 0;
+document.getElementById('total-show').innerText = counter;
 searchButton.addEventListener('click', () => {
     const inpValue = inputName.value;
 
     const url = `http://openlibrary.org/search.json?q=${inpValue}`;
-    fetch(url)
+    console.log();
+    fetch(`${url}`)
         .then(res => res.json())
         .then(data => setData(data))
     inputName.value = '';
@@ -23,15 +27,17 @@ const setData = data => {
     } else {
         msg.classList.add('d-none')
         const book = data.docs;
+        document.getElementById('total-found').innerText = book.length;
         const bookQuantity = book.slice(0, 25)
         const quantity = [...bookQuantity]
         quantity.forEach(book => {
-            // console.log(book);
+            console.log(book);
+            counter = counter + 1;
             let img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
                 // console.log(img)
 
             const { author_name, first_publish_year, title } = book;
-            console.log(author_name, first_publish_year, title);
+            const [...author] = author_name;
             const div = document.createElement('div');
             div.classList.add('col-3')
             div.innerHTML = `
@@ -39,12 +45,13 @@ const setData = data => {
                <img src="${img}" alt="" class="card-img-top">
                 <div class="card-body">
                     <h2 class="text-capitalize">${title}</h2>
-                    <h4 class="card-title text-start">Author:</h4>
+                    <h4 class="card-title text-start">Author:${author}</h4>
                     <p class="text-start">publish date :${first_publish_year}</p>
                 </div>
             </div>
             `
             container.appendChild(div);
         })
+
     }
 }
