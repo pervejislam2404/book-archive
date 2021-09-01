56b89d3cf154f81a421797568861446323176eb4
pervejislam2404 <pervejislam2404@gmail.com>
2021-09-02 04:01:54 +0600

    update

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const searchButton = document.getElementById('button-search');
 const inputName = document.getElementById('search-meal');
 const container = document.getElementById('display-container');
 const msg = document.getElementById('alert-msg');
-// const loader = document.getElementById('preloader');
+const quantity = document.getElementById('quantity');
 const totalData = document.getElementById('total-found')
 const showData = document.getElementById('total-show')
 
@@ -16,6 +16,7 @@ searchButton.addEventListener('click', () => {
     fetch(`${url}`)
         .then(res => res.json())
         .then(data => setData(data))
+        .catch(err => console.error("something went wrong", err))
     inputName.value = '';
 })
 
@@ -24,15 +25,19 @@ const setData = data => {
     // console.log(data.docs.length)
     if (data.docs.length === 0) {
         msg.classList.remove('d-none')
+        quantity.classList.add('d-none')
+
     } else {
+
         msg.classList.add('d-none')
         const book = data.docs;
         document.getElementById('total-found').innerText = book.length;
         const bookQuantity = book.slice(0, 25)
-        const quantity = [...bookQuantity]
-        quantity.forEach(book => {
+            // const quantity = [...bookQuantity]
+        bookQuantity.forEach(book => {
             console.log(book);
             counter = counter + 1;
+            quantity.classList.remove('d-none')
             let img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
                 // console.log(img)
 
@@ -42,11 +47,11 @@ const setData = data => {
             div.classList.add('col-3')
             div.innerHTML = `
             <div class="card p-0 col">
-               <img src="${img}" alt="" class="card-img-top">
+               <img src="${img}" alt="" height="300"; class="card-img-top">
                 <div class="card-body">
                     <h2 class="text-capitalize">${title}</h2>
-                    <h4 class="card-title text-start">Author:${author}</h4>
-                    <p class="text-start">publish date :${first_publish_year}</p>
+                    <h4 class="card-title text-start">Author:${author[0]}</h4>
+                    <p class="text-start">published in ${first_publish_year}</p>
                 </div>
             </div>
             `
