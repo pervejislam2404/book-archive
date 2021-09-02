af6ad66f0862b9fc79f94a4abf400eec8eb1ef89
pervejislam2404 <pervejislam2404@gmail.com>
2021-09-02 18:29:51 +0600

    update

--- js/index.js
+++ js/index.js
@@ -10,8 +10,8 @@ let counter = 0;
 document.getElementById('total-show').innerText = counter;
 searchButton.addEventListener('click', () => {
     const inpValue = inputName.value;
-
-    const url = `http://openlibrary.org/search.json?q=${inpValue}`;
+    // data-fetching
+    const url = `https://openlibrary.org/search.json?q=${inpValue}`;
     console.log();
     fetch(url)
         .then(res => res.json())
@@ -21,9 +21,11 @@ searchButton.addEventListener('click', () => {
 })
 
 const setData = data => {
+    // clearing-past-data-from-main-container    
     container.textContent = '';
-    // console.log(data.docs.length)
+    //   checking-the-fetching-data
     if (data.docs.length === 0) {
+        // error-msg
         msg.classList.remove('d-none')
         quantity.classList.add('d-none')
 
@@ -33,15 +35,17 @@ const setData = data => {
         const book = data.docs;
         document.getElementById('total-found').innerText = book.length;
         const bookQuantity = book.slice(0, 25)
-            // const quantity = [...bookQuantity]
+            //    set-data-to-main-container
         bookQuantity.forEach(book => {
-            console.log(book);
+            console.log(book)
+                //    showed-book-quantity
             counter = counter + 1;
             quantity.classList.remove('d-none')
             let img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-                // console.log(img)
-            const { author_name, first_publish_year, title } = book;
+                //    object-destructuring
+            const { author_name, first_publish_year, publisher, title } = book;
             const author = author_name[0];
+            // making-card-for-each-book
             const div = document.createElement('div');
             div.classList.add('col-3')
             div.innerHTML = `
@@ -49,8 +53,9 @@ const setData = data => {
                <img src="${img}" alt="" height="300"; class="card-img-top">
                 <div class="card-body">
                     <h3 class="text-capitalize">${title}</h3>
-                    <h5 class="card-title text-start">Author:${!author? unknown : author}</h5>
-                    <p class="text-start">published in ${first_publish_year}</p>
+                    <h5 class="card-title text-start">Author: ${!author? unknown : author}</h5>
+                    <p class="text-start">Publisher : ${publisher}</p>
+                    <p class="text-start">Published in ${first_publish_year}</p>
                 </div>
             </div>
             `
